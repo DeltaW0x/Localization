032189651ee5abd1b55f03b06b5d2042c39e05d9
DeltaW0x <98419797+DeltaW0x@users.noreply.github.com>
2025-05-25 23:30:28 +0200

    use tsv file instead of csv

--- generate_json.py
+++ generate_json.py
@@ -3,7 +3,7 @@ import csv
 import json
 
 
-with open(sys.argv[1], mode='r', newline='', encoding='utf-8') as csvfile:
+with open(sys.argv[1], delimiter='\t', mode='r', newline='', quotechar='"' ,encoding='utf-8') as csvfile:
     data = list(csv.DictReader(csvfile))
 
 with open(sys.argv[2], mode='w', encoding='utf-8') as jsonfile:
